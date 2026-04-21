49420cffc944081156089647dd31aec1b50bef72
Nezu SAS <nezu.ec@nezuecuador.com>
2026-04-21 00:42:31 -0500

    finalize dashboards module for production with i18n consolidation and hardening

--- apps/operator-console/src/views/dashboards/WidgetInspector.tsx
+++ apps/operator-console/src/views/dashboards/WidgetInspector.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { isDeviceActive, sanitizeWidget } from './dashboardUtils';
+import { sanitizeWidget } from './dashboardUtils';
 import { cn } from '../../lib/utils';
 import type { DashboardWidget, DashboardWidgetConfig } from './types';
 import { 
